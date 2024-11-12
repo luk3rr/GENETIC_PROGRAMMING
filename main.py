4c4d67cef4cd1a89136ce6e1f8b52ef1fff1f99d
luk3rr <araujolucas@dcc.ufmg.br>
2024-11-12 11:31:22 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Filename: main.py
+# Created on: November 12, 2024
+# Author: Lucas Araújo <araujolucas@dcc.ufmg.br>
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ parameters.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# Filename: parameters.py
+# Created on: November 12, 2024
+# Author: Lucas Araújo <araujolucas@dcc.ufmg.br>
+
+TREE_MAX_DEPTH = 7
